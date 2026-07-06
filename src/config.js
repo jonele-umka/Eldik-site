e0a1d76a5dc89a0586da2eb918bf09fec128f1e4
umka <koichiev.umka@gmail.com>
2026-07-06 21:14:28 +0600

    update env setup

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-export const DEFAULT_API_URL =
-  "https://script.google.com/macros/s/AKfycbx2GuP_69w9FC1pUc4NJ_7IRkeRKqdC01nBZ5QnhMAse8-iYCKvzRm2anrpgE_votfS/exec";
+export const DEFAULT_API_URL = import.meta.env.VITE_API_URL;
